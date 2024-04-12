b851bba4108e103cd339704b69cf6705be8a6674
kacperkuznik <kuznikkacper@gmail.com>
2024-04-12 10:48:16 +0200

    getting crypto data from binance

---
+++ get_binance_data.py
@@ -0,0 +1,34 @@
+import requests
+import datetime
+
+"""
+example api response
+[
+  [
+    1499040000000,      // Kline open time
+    "0.01634790",       // Open price
+    "0.80000000",       // High price
+    "0.01575800",       // Low price
+    "0.01577100",       // Close price
+    "148976.11427815",  // Volume
+    1499644799999,      // Kline Close time
+    "2434.19055334",    // Quote asset volume
+    308,                // Number of trades
+    "1756.87402397",    // Taker buy base asset volume
+    "28.46694368",      // Taker buy quote asset volume
+    "0"                 // Unused field, ignore.
+  ]
+]
+"""
+
+def get_data(start_time, symbol):
+  unix_start_time = int(start_time.strftime("%s")) * 1000 
+  res = requests.get('https://www.binance.com/api/v3/klines', params={"symbol": symbol, "interval": "1d", "startTime": unix_start_time})
+
+  return res.json()
+
+start_time = datetime.datetime(2024, 4, 10)
+symbol = "BTCUSDT"
+
+for kline in get_data(start_time, symbol):
+    print(kline)
